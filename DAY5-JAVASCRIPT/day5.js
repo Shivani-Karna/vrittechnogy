b20e5f053a1021c6077ac2f94d057e93514ae8c8
Sheewani <cwanikarna@gmail.com>
2022-12-13 17:46:40 +0545

    Basic of Javascript

--- DAY5-JAVASCRIPT/day5.js
+++ DAY5-JAVASCRIPT/day5.js
@@ -1,1 +1,54 @@
-kjhkj
+// single line comment
+// declaring variable
+// var a=1;
+//global scope huncha
+//reassign and redeclare both garna milyo
+
+// let b=2;
+//reassign garna mildaina
+//redeclare garna mildaina
+// const c=5;
+//change garna mildaina euta matra rakhna milcha
+//(reassign garna mildaina)
+//redeclare garna mildaina
+// alert(a+b);
+// let a =10;
+// let name
+// let test;
+// prompt(test);
+// console.log("HI its me iam the problem its me");
+// let a =10;
+// let b= 20;
+// let c= a**b;
+
+//power ho.
+// let (a == b);
+// alert(a==b);
+//double equals is used to check the value only
+// alert (a===b);
+//triple equals is used to check the value and datatype
+// let t= false && true;
+// alert(t);
+// let isLoggedIn= true;
+// let isSuperUser= true;
+//Logical AND
+// let test = false || true;
+// Not 
+// let test1 = !true;
+// alert(test1);
+//ARRAY
+// let array=[1,2,3,4,5,6,7];
+    // alert(array[2]);
+//OBJECTS
+// let object1 = { name:'shivani', age:24};
+//             //    key : value   
+//    console.log(object1["age"],"object1"); 
+//    console.log(object1.name,"object1");     
+//EXAMPLE OF OBJECTS
+let fruits={first:'Mango',
+second:'Apple',
+third:'grapes'};
+console.log(fruits);         
+
+
+
